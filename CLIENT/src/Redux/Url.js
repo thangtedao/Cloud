d612d1485ce89688d62ac0ae310b2bf883ffd85c
XuanThang <thangtedao@gmail.com>
2023-12-21 11:01:56 +0700

    web

--- CLIENT/src/Redux/Url.js
+++ CLIENT/src/Redux/Url.js
@@ -1,2 +1,3 @@
 //export const URL=process.env.REACT_APP_SERVER_URL;
-export const URL='http://localhost:5000';
+// export const URL='http://localhost:5000';
+export const URL = "http://ec2-34-201-50-207.compute-1.amazonaws.com:5000";
